ba290a86b46b77a735a4d3251aea16685554f778
Michael L Perry <michael@qedcode.com>
2014-12-28 15:33:59 -0600

    Added logic PCL.

---
+++ CardBoard/CardBoard.Logic/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardBoard.Logic
+{
+    public class Message
+    {
+        public MessageHash Hash { get; set; }
+        public List<MessageHash> Predecessors { get; set; }
+        public string Type { get; set; }
+        public JsonDocument Body { get; set; }
+    }
+}
