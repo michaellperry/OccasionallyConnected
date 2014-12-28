d5b2e33e699bdb5b28989a7131a20f3791d3e3d5
Michael L Perry <michael@qedcode.com>
2014-12-28 16:26:49 -0600

    Test that a card is created.

---
+++ CardBoard/CardBoard.Logic/Models/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardBoard.Messages;
+
+namespace CardBoard.Models
+{
+    public class Application
+    {
+        public Board Board { get; set; }
+
+        public void ReceiveMessage(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CardBoard/CardBoard.Test/MessagingTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CardBoard.Models;
+using Newtonsoft.Json.Linq;
+using CardBoard.Messages;
+using System.Collections.Generic;
+using FluentAssertions;
+
+namespace CardBoard.Test
+{
+    [TestClass]
+    public class MessagingTests
+    {
+        private Application _application;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _application = new Application();
+        }
+
+        [TestMethod]
+        public void CardCreatedOnce()
+        {
+            JObject body = new JObject();
+
+            _application.ReceiveMessage(new Message
+            {
+                Type = "CardCreated",
+                Predecessors = new List<MessageHash>(),
+                Body = body,
+                Hash = new MessageHash()
+            });
+
+            _application.Board.Cards.Count.Should().Be(1);
+        }
+    }
+}
