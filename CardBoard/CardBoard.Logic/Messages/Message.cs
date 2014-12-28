fe8ffd2f28417bfa1bb7fc7bf8358d62d5c7a6a5
Michael L Perry <michael@qedcode.com>
2014-12-28 15:53:29 -0600

    Sketch of the model.

--- CardBoard/CardBoard.Logic/Messages/Message.cs
+++ CardBoard/CardBoard.Logic/Messages/Message.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace CardBoard.Logic
+namespace CardBoard.Messages
 {
     public class Message
     {
         public MessageHash Hash { get; set; }
         public List<MessageHash> Predecessors { get; set; }
         public string Type { get; set; }
-        public JsonDocument Body { get; set; }
+        public JObject Body { get; set; }
     }
 }
---
+++ CardBoard/CardBoard.Logic/Models/Card.cs
@@ -0,0 +1,16 @@
+﻿using CardBoard.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardBoard.Models
+{
+    public class Card
+    {
+        public Guid Id { get; set; }
+        public List<Candidate<string>> Text { get; set; }
+        public List<Candidate<Column>> Column { get; set; }
+    }
+}
