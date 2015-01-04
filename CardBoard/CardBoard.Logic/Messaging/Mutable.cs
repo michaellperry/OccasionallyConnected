97f0535507b8903dd4764255bb3ebbabeae03aef
Michael L Perry <michael@qedcode.com>
2015-01-04 16:56:18 -0600

    Added candidate and mutable.

---
+++ CardBoard/CardBoard.Logic/Messaging/Mutable.cs
@@ -0,0 +1,63 @@
+﻿using Assisticant.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CardBoard.Messaging
+{
+    public class Mutable<T>
+    {
+        private HashSet<MessageHash> _predecessors = new HashSet<MessageHash>();
+        private ObservableList<Candidate<T>> _candidates = new ObservableList<Candidate<T>>();
+        
+        public IEnumerable<Candidate<T>> Candidates
+        {
+            get { return _candidates; }
+        }
+
+        public Message CreateMessage(
+            string messageType,
+            Guid objectId,
+            T value)
+        {
+            return Message.CreateMessage(
+                messageType,
+                _candidates.Select(t => t.MessageHash),
+                objectId,
+                new { Value = value });
+        }
+
+        public void HandleMessage(Message message)
+        {
+            var messageHash = message.Hash;
+            T value = (T)message.Body.Value;
+            var predecessors = message.Predecessors;
+
+            if (!_predecessors.Contains(messageHash))
+            {
+                _candidates.Add(new Candidate<T>(messageHash, value));
+            }
+
+            var newPredecessors = predecessors.Except(_predecessors);
+            _candidates.RemoveAll(c => newPredecessors.Contains(c.MessageHash));
+
+            foreach (var predecessor in newPredecessors)
+                _predecessors.Add(predecessor);
+        }
+
+        public void HandleAllMessages(IEnumerable<Message> messages)
+        {
+            var predecessors = messages
+                .SelectMany(m => m.Predecessors)
+                .ToLookup(h => h);
+            var candidates = messages
+                .Where(m => !predecessors.Contains(m.Hash))
+                .Select(m => new Candidate<T>(m.Hash, (T)m.Body.Value));
+
+            _candidates.RemoveAll(c => predecessors.Contains(c.MessageHash));
+            foreach (var pair in predecessors)
+                _predecessors.Add(pair.Key);
+            _candidates.AddRange(candidates);
+        }
+    }
+}
