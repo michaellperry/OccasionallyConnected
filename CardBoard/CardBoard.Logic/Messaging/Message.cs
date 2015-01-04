144bb45bb7d0f7dd770bd1b60e6bd513b46abd27
Michael L Perry <michael@qedcode.com>
2015-01-04 16:29:53 -0600

    Message hash and predecessors.

--- CardBoard/CardBoard.Logic/Messaging/Message.cs
+++ CardBoard/CardBoard.Logic/Messaging/Message.cs
@@ -1,28 +1,45 @@
 ﻿using Newtonsoft.Json;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.IO;
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Dynamic;
+using System.IO;
+using System.Linq;
 
 namespace CardBoard.Messaging
 {
     public class Message
     {
         private readonly string _type;
+        private readonly ImmutableList<MessageHash> _predecessors;
         private readonly Guid _objectId;
         private readonly ExpandoObject _body;
+        private readonly MessageHash _hash;
 
         private Message(
             string type,
+            ImmutableList<MessageHash> predecessors,
             Guid objectId,
-            ExpandoObject body)
+            ExpandoObject body,
+            MessageHash hash)
         {
             _type = type;
+            _predecessors = predecessors;
             _objectId = objectId;
             _body = body;
+            _hash = hash;
         }
 
         public string Type
         {
             get { return _type; }
+        }
+
+        public IImmutableList<MessageHash> Predecessors
+        {
+            get { return _predecessors; }
         }
 
         public Guid ObjectId
@@ -35,19 +52,59 @@ namespace CardBoard.Messaging
             get { return _body; }
         }
 
+        public MessageHash Hash
+        {
+            get { return _hash; }
+        }
+
         public static Message CreateMessage(
             string messageType,
+            Guid objectId,
+            object body)
+        {
+            return CreateMessage(messageType, new List<MessageHash>(), objectId, body);
+        }
+
+        public static Message CreateMessage(
+            string messageType,
+            IEnumerable<MessageHash> predecessors,
             Guid objectId,
             object body)
         {
             // Convert the anonymous typed object to an ExpandoObject.
             var expandoBody = JsonConvert.DeserializeObject<ExpandoObject>(
                 JsonConvert.SerializeObject(body));
+            object document = new
+            {
+                MessageType = messageType,
+                Predecessors = predecessors
+                    .Select(p => p.ToString())
+                    .ToArray(),
+                ObjectId = objectId,
+                Body = expandoBody
+            };
+            var messageHash = new MessageHash(ComputeHash(document));
 
             return new Message(
                 messageType,
+                predecessors.ToImmutableList(),
                 objectId,
-                expandoBody);
+                expandoBody,
+                messageHash);
+        }
+
+        private static byte[] ComputeHash(object document)
+        {
+            var sha = new Sha256Digest();
+            var stream = new DigestStream(new MemoryStream(), null, sha);
+            using (var writer = new StreamWriter(stream))
+            {
+                string mementoToString = JsonConvert.SerializeObject(document);
+                writer.Write(mementoToString);
+            }
+            byte[] buffer = new byte[sha.GetDigestSize()];
+            sha.DoFinal(buffer, 0);
+            return buffer;
         }
     }
 }
