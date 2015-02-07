2cff000fc8cb46e75d1b82ebc6a8cb3bd00f3a93
Michael L Perry <michael@qedcode.com>
2015-02-07 17:32:32 -0600

    Prepare to send all messages .

--- CardBoard/CardBoard.Logic/Messaging/HttpMessagePump.cs
+++ CardBoard/CardBoard.Logic/Messaging/HttpMessagePump.cs
@@ -21,57 +21,33 @@ namespace CardBoard.Messaging
             _messageQueue = messageQueue;
         }
 
-        public void Enqueue(Message message)
-        {
-            lock (this)
-            {
-                _queue = _queue.Enqueue(message);
-            }
-            Perform(() => SendAndReceiveMessagesInternalAsync());
-        }
-
-        private async Task SendAndReceiveMessagesInternalAsync()
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                while (true)
-                {
-                    var queue = _queue;
-                    if (queue.IsEmpty)
-                        return;
-                    var message = queue.Peek();
-
-                    await SendMessageAsync(client, message);
-
-                    lock (this)
-                    {
-                        _queue = _queue.Dequeue();
-                    }
-                    _messageQueue.Confirm(message);
-                }
-            }
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        public void SendAndReceiveMessages()
+        public void SendAllMessages(ImmutableList<Message> messages)
         {
             throw new NotImplementedException();
         }
 
-        public void SendAllMessages(ImmutableList<Message> messages)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public void Enqueue(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendAndReceiveMessages()
         {
             throw new NotImplementedException();
         }
@@ -79,7 +55,6 @@ namespace CardBoard.Messaging
         private async Task SendMessageAsync(HttpClient client, Message message)
         {
             // TODO
-            throw new CommunicationException("Simulated communication error.");
         }
     }
 }
