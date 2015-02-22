568f480512fea7422039a53b7f5551e13d6f584a
Michael L Perry <michael@qedcode.com>
2015-02-21 22:54:07 -0600

    Send push notifications from the distributor.

--- CardBoard/CardBoard.Distributor/Controllers/DistributorController.cs
+++ CardBoard/CardBoard.Distributor/Controllers/DistributorController.cs
@@ -7,16 +7,20 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using CardBoard.Distributor.Notification;
+using System.Threading.Tasks;
 
 namespace CardBoard.Distributor.Controllers
 {
     public class DistributorController : ApiController
     {
         private AzureStorageProvider _storage = new AzureStorageProvider();
+        private AzurePushNotificationProvider _pushNotification = new AzurePushNotificationProvider();
 
-        public void Post(string topic, [FromBody]MessageMemento message)
+        public async Task Post(string topic, [FromBody]MessageMemento message)
         {
             _storage.WriteMessage(topic, message);
+            await _pushNotification.SendNotificationAsync(topic, message);
         }
 
         public PageMemento Get(string topic, string bookmark)
---
+++ CardBoard/CardBoard.Distributor/Notification/AzurePushNotificationProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CardBoard.Protocol;
+using System.Web.Configuration;
+using Microsoft.ServiceBus.Notifications;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace CardBoard.Distributor.Notification
+{
+    public class AzurePushNotificationProvider
+    {
+        public async Task SendNotificationAsync(string topic, MessageMemento message)
+        {
+            var connectionString = WebConfigurationManager.AppSettings["NotificationConnectionString"];
+            NotificationHubClient hub = NotificationHubClient
+                .CreateClientFromConnectionString(
+                    connectionString, "occdist");
+
+            var json = JsonConvert.SerializeObject(message);
+            var notification = new WindowsNotification(json,
+                new Dictionary<string, string> {
+				{"X-WNS-Type", "wns/raw"}
+			});
+            await hub.SendNotificationAsync(notification, topic);
+        }
+    }
+}
