969acd4f2235eb1c139f1b0f08616a8506be1762
Michael L Perry <michael@qedcode.com>
2015-04-04 11:45:32 -0500

    Added Rover Mobile package.

---
+++ FieldService/FieldService/FieldService.Windows/ViewModelLocator.cs
@@ -0,0 +1,13 @@
+﻿using Assisticant;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldService
+{
+    class ViewModelLocator : ViewModelLocatorBase
+    {
+    }
+}
