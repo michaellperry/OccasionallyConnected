be54702992c520c1c5ab8767f5fe31a141e18f0c
Michael L Perry <michael@qedcode.com>
2015-04-04 18:20:45 -0500

    Initialize the services of the Application.

--- FieldService/FieldService.Logic/Initializer.cs
+++ FieldService/FieldService.Logic/Initializer.cs
@@ -1,5 +1,6 @@
 ﻿using FieldService.Models;
 using RoverMob;
+using RoverMob.Messaging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,17 @@ namespace FieldService
     {
         public static Application<Technician> LoadApplication()
         {
-            var application = new Application<Technician>();
+            string folderName = "MyApp";
+            var store = new FileMessageStore(folderName);
+            var queue = new FileMessageQueue(folderName);
+            var bookmarkStore = new FileBookmarkStore(folderName);
+            var pump = new HttpMessagePump(
+                new Uri("http://host/api/distributor/", UriKind.Absolute),
+                queue,
+                bookmarkStore);
+            var push = new PushNotificationSubscription();
+            var application = new Application<Technician>(
+                store, queue, pump, push);
             application.Load(new Technician(Guid.NewGuid()));
             return application;
         }
