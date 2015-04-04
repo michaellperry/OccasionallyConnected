ea7005da589c7cf1fa6a12fbf3265b5c67ccb736
Michael L Perry <michael@qedcode.com>
2015-04-03 22:10:27 -0500

    Project structure.

---
+++ FieldService/FieldService.UnitTest/ModelTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace FieldService.UnitTest
+{
+    [TestClass]
+    public class ModelTest
+    {
+        [TestMethod]
+        public void TechnicianHasVisits()
+        {
+            Assert.Fail("Not implemented.");
+        }
+    }
+}
