181b10c45dc46a8a3f7c58c4316b9e0c3d982099
Michael L Perry <michael@qedcode.com>
2015-05-24 14:34:49 -0500

    Added responsibilities to the Visit fact.

--- FieldService/Database/Visit.Procedure.sql
+++ FieldService/Database/Visit.Procedure.sql
@@ -8,11 +8,19 @@ GO
 CREATE PROCEDURE Visit
 	@IncidentId int,
 	@VisitHash varchar(50),
-	@DateOfVisit date
+	@DateOfVisit date,
+	@FollowUpOrderHash varchar(50) NULL
 AS
+	DELETE FROM Reported
+	WHERE Reported.IncidentId = @IncidentId
+
 	INSERT INTO Scheduled
 		(IncidentId, VisitHash, DateOfVisit)
 	VALUES
 		(@IncidentId, @VisitHash, @DateOfVisit)
+
+	DELETE FROM AwaitingFollowUp
+	WHERE AwaitingFollowUp.IncidentId = @IncidentId
+	  AND AwaitingFollowUp.FollowUpOrderHash = @FollowUpOrderHash
 
 GO
