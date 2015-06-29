5580be07f961b2303c10dc98d9bddc28275638c6
Michael L Perry <michael@qedcode.com>
2015-06-28 19:56:40 -0500

    Await getting an access token.

--- FieldService/FieldService/FieldService.Shared/Common/AuthenticationManager.cs
+++ FieldService/FieldService/FieldService.Shared/Common/AuthenticationManager.cs
@@ -3,6 +3,8 @@ using Newtonsoft.Json.Linq;
 using RoverMob.Tasks;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Security.Authentication.Web;
@@ -11,9 +13,28 @@ namespace FieldService.Common
 {
     public class AuthenticationManager : Process
     {
-        private string _accessToken = result.AccessToken;
+        private string _accessToken;
+        private ImmutableList<TaskCompletionSource<string>> _accessTokenCompletions =
+            ImmutableList<TaskCompletionSource<string>>.Empty;
+
         public AuthenticationManager()
         {
+        }
+
+        public Task<string> GetAccessTokenAsync()
+        {
+            if (!String.IsNullOrEmpty(_accessToken))
+                return Task.FromResult(_accessToken);
+
+            var completion = new TaskCompletionSource<string>();
+            lock (this)
+            {
+                _accessTokenCompletions = _accessTokenCompletions.Add(
+                    completion);
+            }
+            Authenticate();
+
+            return completion.Task;
         }
 
         public void Authenticate()
@@ -26,18 +47,50 @@ namespace FieldService.Common
 
         private async Task InternalAuthenticateAsync()
         {
+            if (!String.IsNullOrEmpty(_accessToken))
+            {
+                ReceiveAccessToken(_accessToken);
+                return;
+            }
+
             string authority = "https://login.windows.net/mperryfacetedworlds.onmicrosoft.com";
             string resource = "https://mperryfacetedworlds.onmicrosoft.com/fieldservicedistributor";
             string clientId = "e888dc86-3f7d-40dc-b9ef-296b38b223cc";
             Uri redirectUri = WebAuthenticationBroker.GetCurrentApplicationCallbackUri();
 
 #if WINDOWS_PHONE_APP
+            var context = await AuthenticationContext.CreateAsync(
+                authority);
+            var result = await context.AcquireTokenSilentAsync(
+                resource, clientId);
+
+            if (result != null)
+                ReceiveAccessToken(result.AccessToken);
+            else
+                context.AcquireTokenAndContinue(
+                    resource, clientId, redirectUri,
+                    r => ReceiveAccessToken(r.AccessToken));
 #else
             var context = new AuthenticationContext(authority);
             var result = await context.AcquireTokenAsync(
                 resource, clientId, redirectUri);
-#endif
 
+            ReceiveAccessToken(result.AccessToken);
+#endif
+        }
+
+        private void ReceiveAccessToken(string accessToken)
+        {
+            _accessToken = accessToken;
+            ImmutableList<TaskCompletionSource<string>> completions;
+            lock (this)
+            {
+                completions = _accessTokenCompletions;
+                _accessTokenCompletions = ImmutableList<
+                    TaskCompletionSource<string>>.Empty;
+            }
+            foreach (var completion in completions)
+                completion.SetResult(_accessToken);
         }
     }
 }
